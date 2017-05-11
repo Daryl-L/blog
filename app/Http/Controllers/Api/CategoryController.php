8438dd8dcce4dba83da6d57c9ce83a9cb189a4ce
daryl <sai9375@hotmail.com>
2017-05-11 22:14:11 +0800

    feat:add category controller

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    /**
+     * App\Category
+     */
+    protected $category;
+
+    public function __construct(\App\Category $category)
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->category->withCount('articles')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $category = $this->category->where('name', $request->input('name'))->firstOrNew();
+
+        if (!$category->id) {
+            $category->save();
+
+            return response()->json([
+                'msg' => 'Category created successfully.'
+            ], 201);
+        } else {
+            return response()->json([
+                'error' => 'Category already exsits.';
+            ], 409);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            $category = $this->category->with('articles')->findOrFail($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json([
+                'error' => 'Category not found.',
+            ], 404);
+        }
+
+        return $category;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $category = $this->category->with('articles')->findOrFail($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json([
+                'error' => 'Category not found.',
+            ], 404);
+        }
+
+        $category->name = $request->input('name');
+        $category->save();
+
+        return response()->json([
+            'msg' => 'Category updated successfully.',
+        ], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $category = $this->category->with('articles')->findOrFail($id);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json([
+                'error' => 'Category not found.',
+            ], 404);
+        }
+
+        $category->delete();
+
+        return response()->json([], 204);
+    }
+}
--- database/migrations/2017_05_10_150846_create_categories_table.php
+++ database/migrations/2017_05_10_150846_create_categories_table.php
@@ -12,7 +12,7 @@ class CreateCategoriesTable extends Migration
      * @return void
      */
     public function up()
-    {s
+    {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
