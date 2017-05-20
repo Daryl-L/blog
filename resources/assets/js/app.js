ee6c9e9f92c75ac7a2cc63527680cf2ab0fc1739
daryl <sai9375@hotmail.com>
2017-05-20 23:22:31 +0800

    feat:use elemetui.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,6 @@
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router';
+import 'element-ui/lib/theme-default/index.css'
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -13,9 +16,25 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+Vue.use(VueRouter)
+Vue.use(ElementUI)
+
 Vue.component('example', require('./components/Example.vue'));
-Vue.use(require('vue-material'))
+
+const routes = [
+    { path: '/', component: require('./components/admin/Home.vue'), 
+        children: [
+            { path: 'article', component: require('./components/admin/Article.vue') }
+        ]
+    }
+]
+const router = new VueRouter({
+    routes
+})
+
+console.log(router)
 
 const app = new Vue({
+    router,
     el: '#app'
 });
