3b816e89073c6ebbb4952d34c4ab6bc413552f2f
daryl <sai9375@hotmail.com>
2017-05-22 23:51:41 +0800

    feat:add three routes in api with get article list, publish article and delete article

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,12 @@ Route::middleware('auth.basic.once')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/article', 'Api\ArticleController@index');
+
 Route::post('/article', 'Api\ArticleController@store');
+
+Route::patch('/article/{id}', 'Api\ArticleController@publish')->where('id', '[0-9]+');
+
+Route::delete('/article/{id}', 'Api\ArticleController@destroy')->where('id', '[0-9]+');
 
 Route::get('/category', 'Api\CategoryController@index');
